d9108e63a48d048e03c288d7e2075d47b24fc9bd
mithiljoshi <55636989+mithiljoshi@users.noreply.github.com>
2021-05-12 12:03:23 +0530

    Add files via upload

---
+++ planet_data.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+sun = {
+        'name': 'Sun',
+        'mass': 1.989e30,
+        'mu': 1.32712e11,
+        'radius': 695700.0
+}
+earth = {
+        'name': 'Earth',
+        'mass': 5.972e24,
+        'mu': 5.972e24*6.67408e-11,
+        'radius': 6378.0
+}
+
+
+# In[ ]:
+
+
+
+
